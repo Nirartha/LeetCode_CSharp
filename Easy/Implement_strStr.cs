98b051b3439a2fb0b0f678cf6bf2f7d95fa474be
Dennis Chen <nirartha3214@gmail.com>
2021-10-06 00:34:53 +0800

    Add files via upload
    28. Implement strStr()

---
+++ Easy/Implement_strStr.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode
+{
+    public class Implement_strStr
+    {
+        public Implement_strStr() { }
+
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0) return 0;
+            if (haystack.Length == 0) return -1;
+            if (haystack == needle) return 0;
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack.Substring(i, needle.Length) == needle)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
