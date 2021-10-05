fa9390dde7cf90fb11302cba148e4ed9c6ae4fa2
Dennis Chen <nirartha3214@gmail.com>
2021-10-06 00:40:21 +0800

    Add files via upload
    re-upload Longest Common Prefix

---
+++ Easy/Easy_LongestCommonPrefix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Easy_LongestCommonPrefix
+    {
+        public Easy_LongestCommonPrefix() { }
+
+        public string LongestCommonPrefix(string[] strs)
+        {
+
+            if (strs.Length == 0)
+                return "";
+
+            StringBuilder prefix = new StringBuilder(strs[0]);
+
+            foreach (string word in strs)
+            {
+
+                if (prefix.Length > word.Length)
+                    prefix.Length = word.Length;
+
+                for (int i = 0; i < prefix.Length; i++)
+                {
+                    if (word[i] != prefix[i])
+                    {
+                        prefix.Length = i;
+                        break;
+                    }
+                }
+
+            }
+            return prefix.ToString();
+        }
+    }
+}
