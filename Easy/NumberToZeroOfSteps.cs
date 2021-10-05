39469165a4b813bfb0ef274f10d69da3733330a6
Dennis Chen <nirartha3214@gmail.com>
2021-10-06 00:35:17 +0800

    Add files via upload
    1342. Number of Steps to Reduce a Number to Zero

---
+++ Easy/NumberToZeroOfSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LeetCode
+{
+    public class NumberToZeroOfSteps
+    {
+        public NumberToZeroOfSteps() { }
+
+        public int NumberOfSteps(int num)
+        {
+            if (num == 0) return 0;
+            else if (num % 2 == 0) return 1 + NumberOfSteps(num / 2);
+            else return 1 + NumberOfSteps(num - 1);
+        }
+    }
+}
