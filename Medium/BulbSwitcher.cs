4ce82731c3bd0b7c6a137b2428c3fcb0bbd73919
Dennis Chen <nirartha3214@gmail.com>
2021-10-12 00:18:51 +0800

    319. Bulb Switcher

---
+++ Medium/BulbSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace LeetCode
+{
+    public class BulbSwitcher
+    {
+        public BulbSwitcher() { }
+
+        public int BulbSwitch(int n)
+        {
+            /*
+            if (n == 0) return 0;
+
+            //round 0
+            int[] bulbArr = new int[n];
+
+            //round 1: turn on all bulbs
+            for (int x = 0; x < bulbArr.Length; x++) bulbArr[x] = 1;
+
+            if (n > 1)
+            {
+                int round = 2;
+                while (round <= n)
+                {
+                    for (int i = round - 1; i < bulbArr.Length; i += round)
+                    {
+                        if (bulbArr[i] == 1) bulbArr[i] = 0;
+                        else bulbArr[i] = 1;
+                    }
+                    round++;
+                }
+            }
+
+            int sum = 0;
+            Array.ForEach(bulbArr, i => sum += i);
+            return sum;
+            */
+
+
+            return (int)Math.Sqrt(n);
+        }
+    }
+}
