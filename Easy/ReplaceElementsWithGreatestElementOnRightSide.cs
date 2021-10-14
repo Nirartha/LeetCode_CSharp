6d54d162910065504aa8d8787403a85c700496d7
Dennis Chen <nirartha3214@gmail.com>
2021-10-14 16:10:24 +0800

    1299. Replace Elements with Greatest Element on Right Side

---
+++ Easy/ReplaceElementsWithGreatestElementOnRightSide.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class ReplaceElementsWithGreatestElementOnRightSide
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            if (arr.Length == 1) arr[0] = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == arr.Length - 1) arr[arr.Length - 1] = -1;
+                else
+                {
+                    int tempGreatestNbr = 0;
+                    for (int j = i + 1; j < arr.Length; j++) if (arr[j] > tempGreatestNbr) tempGreatestNbr = arr[j];
+                    arr[i] = tempGreatestNbr;
+                }
+            }
+
+            return arr;
+
+
+            /* for loop from end to begin
+            int max = -1;
+            var result = new int[n];
+            for(int i = arr.Length - 1; i >= 0; i--)
+            {
+                result[i] = max;
+                max = Math.Max(max, arr[i]);
+            }
+        
+            return result;
+            */
+
+        }
+    }
+}
