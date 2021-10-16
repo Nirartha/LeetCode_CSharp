c7dd5eb85f1de0fed4c7d2d23c64b952188e82a7
Dennis Chen <nirartha3214@gmail.com>
2021-10-16 23:26:27 +0800

    442.Find All Duplicates in an Array

---
+++ Easy/FindAllDuplicatesinArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class FindAllDuplicatesinArray
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            if (nums.Length <= 1) return new int[] { };
+
+            List<int> result = new List<int>();
+            Dictionary<int, int> intKeyValues = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (intKeyValues.ContainsKey(num)) intKeyValues[num]++;
+                else intKeyValues.Add(num, 1);
+
+                if (intKeyValues[num] == 2) result.Add(num);
+            }
+
+            return result;
+        }
+    }
+}
