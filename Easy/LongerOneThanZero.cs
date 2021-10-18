f2ade593b81df8f9861a840cd4eda0e29db9f969
Dennis Chen <nirartha3214@gmail.com>
2021-10-19 01:46:04 +0800

    1869. Longer Contiguous Segments of Ones than Zeros
    Runtime: 68 ms, faster than 96.88% of C# online submissions for Longer Contiguous Segments of Ones than Zeros.
    Memory Usage: 37.6 MB, less than 9.38% of C# online submissions for Longer Contiguous Segments of Ones than Zeros.

---
+++ Easy/LongerOneThanZero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode
+{
+    public class LongerOneThanZero
+    {
+        public LongerOneThanZero() { }
+        
+        public bool CheckZeroOnes(string s)
+        {
+            if (s.Length == 0) return false;
+            if (s.Length == 1 && s == "1") return true;
+
+            int Zeros = 0, tempZeros = 0;
+            int Ones = 0, tempOnes = 0;
+            foreach (char i in s.ToCharArray())
+            {
+                if (i == '0')
+                {
+                    tempZeros++;
+                    tempOnes = 0;
+                }
+                else
+                {
+                    tempOnes++;
+                    tempZeros = 0;
+                }
+                if (tempZeros > Zeros) Zeros = tempZeros;
+                if (tempOnes > Ones) Ones = tempOnes;
+            }
+            
+            return Ones > Zeros ? true : false;
+        }
+
+    }
+}
