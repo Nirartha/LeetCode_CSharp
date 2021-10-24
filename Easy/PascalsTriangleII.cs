600ff6c9cf0fad5a0095c88439aa0613bde8149e
Dennis Chen <nirartha3214@gmail.com>
2021-10-24 10:50:14 +0800

    119. Pascal's Triangle II

---
+++ Easy/PascalsTriangleII.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class PascalsTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            IList<int> prev = new List<int> { 1 };
+
+            for (int i = 1; i <= rowIndex; i++)
+            {
+                IList<int> cur = new List<int> { 1 };
+
+                for (int j = 1; j < i; j++)
+                    cur.Add(prev[j - 1] + prev[j]);
+
+                cur.Add(1);
+                prev = cur;
+            }
+            
+            return prev;
+        }
+    }
+}
