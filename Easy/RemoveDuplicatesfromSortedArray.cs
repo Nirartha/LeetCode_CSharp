2aa7bd1f7ab6f070a629c58b740da1d406769808
Dennis Chen <nirartha3214@gmail.com>
2021-10-24 10:49:26 +0800

    26. Remove Duplicates from Sorted Array

---
+++ Easy/RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class RemoveDuplicatesfromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 2) return nums.Length;
+            int curIndex = 1;
+            int curNum = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > curNum)
+                {
+                    nums[curIndex] = nums[i];
+                    curNum = nums[i];
+                    curIndex++;
+                }
+            }
+            return curIndex;
+        }
+    }
+}
