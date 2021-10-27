15dd6b085b6ad43fdc6561562882d6ae0a4a3e50
Dennis Chen <nirartha3214@gmail.com>
2021-10-28 01:11:21 +0800

    1344. Angle Between Hands of a Clock
    Runtime: 32 ms, faster than 94.41% of C# online submissions for Angle Between Hands of a Clock.
    Memory Usage: 27 MB, less than 6.70% of C# online submissions for Angle Between Hands of a Clock.

---
+++ Easy/AngleBetweenHandsofClock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class AngleBetweenHandsofClock
+    {
+        public double AngleClock(int hour, int minutes)
+        {
+            return Math.Min(Math.Abs((((double)hour / 12 * 360) % 360 + ((double)minutes / 60 * 30)) - ((double)minutes / 60 * 360)), 360 - Math.Abs((((double)hour / 12 * 360) % 360 + ((double)minutes / 60 * 30)) - ((double)minutes / 60 * 360)));
+        }
+    }
+}
