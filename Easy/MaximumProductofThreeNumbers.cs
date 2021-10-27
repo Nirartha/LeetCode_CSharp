1be5e8c6e3ac1b306683ef8d787d71323c407351
Dennis Chen <nirartha3214@gmail.com>
2021-10-27 22:46:47 +0800

    628. Maximum Product of Three Numbers

---
+++ Easy/MaximumProductofThreeNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class MaximumProductofThreeNumbers
+    {
+        public int MaximumProduct(int[] nums)
+        {
+            Array.Sort(nums);
+            return Math.Max(nums[0] * nums[1] * nums[nums.Length - 1], nums[nums.Length - 3] * nums[nums.Length - 2] * nums[nums.Length - 1]);
+        }
+    }
+}
