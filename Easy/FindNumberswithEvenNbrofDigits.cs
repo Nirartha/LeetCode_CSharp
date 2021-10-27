15e02aeac61b02130df1f9fee3c779b2a8acdac3
Dennis Chen <nirartha3214@gmail.com>
2021-10-27 22:46:17 +0800

    1295. Find Numbers with Even Number of Digits

---
+++ Easy/FindNumberswithEvenNbrofDigits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class FindNumberswithEvenNbrofDigits
+    {
+        public int FindNumbers(int[] nums)
+        {
+            return nums.Where(x => x.ToString().Length % 2 == 0).Count(); //Or nums.Count(x => x.ToString().Length % 2 == 0)
+        }
+    }
+}
