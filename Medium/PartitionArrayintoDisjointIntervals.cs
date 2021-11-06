4489625d89cfdfe70c82210cbe3b8c76ac87a654
Dennis Chen <nirartha3214@gmail.com>
2021-11-06 23:52:13 +0800

    915. Partition Array into Disjoint Intervals

---
+++ Medium/PartitionArrayintoDisjointIntervals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class PartitionArrayintoDisjointIntervals
+    {
+        public int PartitionDisjoint(int[] nums)
+        {
+            int leftIndex = 0, curMax = nums[0], leftMax = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < leftMax)
+                {
+                    leftIndex = i;
+                    leftMax = curMax;
+                }
+                curMax = Math.Max(curMax, nums[i]);
+            }
+            return leftIndex + 1;
+        }
+    }
+}
