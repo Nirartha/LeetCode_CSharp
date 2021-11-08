4534e4fdb99ebd53acea04d1fb0b6b98c10cb114
Dennis Chen <nirartha3214@gmail.com>
2021-11-08 10:51:26 +0800

    70. Climbing Stairs

---
+++ Easy/ClimbingStairs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class ClimbingStairs 
+    {
+        public int ClimbStairs(int n)
+        {
+            //Dynamic Programming
+            if (n < 2) return 1;
+            int result = 0;
+            int firstStep = 1;
+            int secondStep = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = firstStep + secondStep;
+                firstStep = secondStep;
+                secondStep = result;
+            }
+
+            return result;
+        }
+    }
+}
