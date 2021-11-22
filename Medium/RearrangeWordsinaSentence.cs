33f82640071f5937e9238a6245de8dd306ac68db
Dennis Chen <nirartha3214@gmail.com>
2021-11-22 16:47:02 +0800

    1451. Rearrange Words in a Sentence

---
+++ Medium/RearrangeWordsinaSentence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class RearrangeWordsinaSentence
+    {
+        public string ArrangeWords(string text)
+        {
+            string[] strArr = text.Split(" ");
+            int maxLength = strArr.Max(x => x.Length);
+            string res = "";
+            for (int i = 1; i <= maxLength; i++)
+            {
+                for (int j = 0; j < strArr.Length; j++)
+                {
+                    if (strArr[j].Length == i) res += strArr[j].ToLower() + " ";
+                }
+            }
+            return res.Substring(0, 1).ToUpper() + res.Substring(1, res.Length - 2);
+        }
+    }
+}
