f6e812877f5528f9d23bc6bde050157f2973069a
Dennis Chen <nirartha3214@gmail.com>
2021-12-26 17:14:33 +0800

    926. Flip String to Monotone Increasing

---
+++ Medium/MinFlipsMonoIncreasing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class MinFlipsMonoIncreasing
+    {
+        public int MinFlipsMonoIncr(string s)
+        {
+            int leftOne = 0;
+            int rightZero = s.Count(c => c == '0');
+            int min = s.Length - rightZero;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1') 
+                {
+                    min = Math.Min(min, leftOne + rightZero);
+                    leftOne++;
+                }
+                else rightZero--;
+            }
+            return min;
+        }
+    }
+}
