28c6451fa7e80668a993ad1b248ef1e3468fe422
Dennis Chen <nirartha3214@gmail.com>
2022-01-09 20:46:44 +0800

    796. Rotate String

---
+++ Easy/StringRotation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class StringRotation
+    {
+        public bool RotateString(string s, string goal)
+        {
+            if (s.Length == 0 || goal.Length == 0) return false;
+            if (s.Length > 100 || goal.Length > 100) return false;
+            if (s.Length != goal.Length) return false;
+
+            string newS = s + s;
+            for (int i = 0; i < newS.Length - goal.Length + 1; i++)
+            {
+                if (newS.Substring(i, goal.Length) == goal) return true;
+            }
+
+            return false;
+        }
+    }
+}
