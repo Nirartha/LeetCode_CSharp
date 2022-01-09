6a193459359efb2706d8a33bc998b0a6b78a9c40
Dennis Chen <nirartha3214@gmail.com>
2022-01-09 21:33:03 +0800

    3. Longest Substring Without Repeating Characters

---
+++ Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+            int result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                result = Math.Max(result, countLength(s.Substring(i)));
+            }
+            return result;
+        }
+
+        int countLength(string s)
+        {
+            char[] charS = s.ToCharArray();
+            Boolean[] booleanArrS = new Boolean[128];
+            int counts = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int val = charS[i];
+                if (booleanArrS[val]) return counts;
+                booleanArrS[val] = true;
+                counts++;
+            }
+            return counts;
+        }
+    }
+}
