2d5519208a104d76b2dc66882efb5b20a2e1dccc
Dennis Chen <nirartha3214@gmail.com>
2022-02-22 22:24:49 +0800

    5. Longest Palindromic Substring
    - still error "Time Limit Exceeded"

---
+++ Medium/SubLongestPalindrome.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class SubLongestPalindrome
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (s.Length == 0) return "";
+            char[] baseStr = s.ToCharArray();
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 1; j <= s.Length - i; j++)
+                {
+                    Console.WriteLine(s.Substring(i, j));
+                    if (checkPalindromeByString(s.Substring(i, j)) && s.Substring(i, j).Length > result.Length) result = s.Substring(i, j);
+                }
+            }
+            return result;
+        }
+
+        public bool checkPalindromeByString(string str)
+        {
+            char[] charArr = str.ToCharArray();
+            if (charArr.Length % 2 == 0)
+            {
+                for (int i = 0; i < charArr.Length/2; i++)
+                {
+                    if (charArr[i] != charArr[charArr.Length - 1 - i]) return false;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < (charArr.Length - 1) / 2; i++)
+                {
+                    if (charArr[i] != charArr[charArr.Length - 1 - i]) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
