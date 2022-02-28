e05d29a94cdbc62e757c06984b0ee2eed2b181cc
Dennis Chen <nirartha3214@gmail.com>
2022-03-01 00:00:39 +0800

    1848. Minimum Distance to the Target Element

---
+++ Easy/MinDis2TargetElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    class MinDis2TargetElement
+    {
+        public int GetMinDistance(int[] nums, int target, int start)
+        {
+            if (nums[start] == target) return 0;
+            else
+            {
+                int result = 0;
+                int leftDis = 0;
+                int rightDis = 0;
+                int index = 1;
+                while (result == 0)
+                {
+                    if (start - index >= 0)
+                    {
+                        leftDis++;
+                        if (nums[start - index] == target) result = leftDis;
+                    }
+
+                    if (start + index < nums.Length)
+                    {
+                        rightDis++;
+                        if (nums[start + index] == target) result = rightDis;
+                    }
+                    index++;
+                }
+                return result;
+            }
+        }
+    }
+}
