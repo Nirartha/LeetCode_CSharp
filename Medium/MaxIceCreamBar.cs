bcd8a5572e37167df131bafebf40170bb0513fd9
Dennis Chen <nirartha3214@gmail.com>
2022-03-01 11:39:04 +0800

    1833. Maximum Ice Cream Bars

---
+++ Medium/MaxIceCreamBar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodePractice
+{
+    /*
+     * 1833. Maximum Ice Cream Bars
+     * Runtime: 332 ms, faster than 72.73% of C# online submissions for Maximum Ice Cream Bars.
+     * Memory Usage: 49.7 MB, less than 54.55% of C# online submissions for Maximum Ice Cream Bars.
+     * 
+     */
+    class MaxIceCreamBar
+    {
+        public int MaxIceCream(int[] costs, int coins)
+        {
+            int result = 0;
+            var newCosts = costs.OrderBy(x => x).ToList();
+            foreach (int ele in newCosts)
+            {
+                if (coins - ele < 0) break;
+                coins -= ele;
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
